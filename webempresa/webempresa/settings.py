2f41fc9e8ad18d671edb43fd3877b103183bfa10
María Del Carmen Martín Rodríguez <mainen1985@gmail.com>
2025-05-23 16:05:04 +0200

    Update settings.py

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x168ig(yukfaedj+x(cdy)hvh_c#_uwac#v4)6m8whz)8rh141"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
